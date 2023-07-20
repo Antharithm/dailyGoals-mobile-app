ff77d8a28e302b99ba412084f51ce51c80a33749
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-07-20 14:27:34 -0400

    basic styling added

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -2,12 +2,15 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
+    // outter box
     <View style={styles.appContainer}>
+      {/* input field */}
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      {/* // goal list box */}
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -16,18 +19,32 @@ export default function App() {
 
 // Import stylesheet for auto completion
 const styles = StyleSheet.create({
+  // outter box
   appContainer: {
-    padding: 50, // pixels
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
+  // input field box
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    margingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
+  // text field box
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "75%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  // goals box
+  goalsContainer: {
+    flex: 5,
   },
 });
